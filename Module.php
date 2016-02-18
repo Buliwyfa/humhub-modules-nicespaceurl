1b36f18a19ff929f756a8885409a099cfd4a126e
Tobias Johannes <tobciu@users.noreply.github.com>
2016-02-18 19:16:15 +0100

    Update Module.php

--- Module.php
+++ Module.php
@@ -8,6 +8,7 @@ class Module extends \humhub\components\Module {
 	public static function examineUrlRules(\yii\base\Event $event) {
 		$rules = Yii::$app->urlManager->rules;
 		foreach ( $rules as $key => $rule ) {
+			// removes humhub url generation for space
 			if ($rule instanceof \humhub\modules\space\components\UrlRule)
 				unset ( $rules [$key] );
 		}
